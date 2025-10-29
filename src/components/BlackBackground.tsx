7ce3e0bae7c330721504c7d74f86a0960f558296
NathanT <nathanthompson256@outlook.com>
2025-10-29 21:51:22 +0000

    fun gsap stuff

--- src/components/BlackBackground.tsx
+++ src/components/BlackBackground.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const BlackBackground = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
+        <div className="min-h-screen bg-nice-black text-white flex flex-col items-center justify-center">
             {children}
         </div>
     );
--- src/components/SimpleHome.tsx
+++ src/components/SimpleHome.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { BlackBackground } from '@/components/BlackBackground';
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Link from "next/link";
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
@@ -11,11 +11,43 @@ export default function SimpleHome() {
         gsap.from('.fade', { opacity: 0 , y : 30, duration: 1, stagger: 0.2});
     }, []);
 
+
+
+    const headingRef = useRef<HTMLHeadingElement>(null)
+
+    const handleMouseEnter = () => {
+        const colorDuration = 0.1;
+
+        gsap.to(headingRef.current, {
+            keyframes: [
+                { color: "#F1E6E4", duration: colorDuration },
+                { color: "#EADAD7", duration: colorDuration },
+                { color: "#E3CDCA", duration: colorDuration },
+                { color: "#DCC0BC", duration: colorDuration }
+            ],
+            repeat: -1,
+            yoyo: true,
+            ease: "power1.inOut",
+        });
+    };
+
+
+    const handleMouseLeave = () => {
+        gsap.killTweensOf(headingRef.current);
+        gsap.to(headingRef.current, { color: "#F1E6E4", duration: 0.5 });
+    };
+
     return (
         <BlackBackground>
             <main className="h-screen flex flex-col justify-center items-center text-center">
-                <h1 className="fade text-5xl font-bold>">Nathan Thompson</h1>
-                <p className="fade text-3xl mt-7 text-grey-600">Hi, I&#39;m Nathan</p>
+                <h1 ref={headingRef}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    className="fade text-5xl font-bold cursor-pointer"
+                >
+                    Hey, I’m Nathan
+                </h1>
+                <p className="fade text-3xl mt-7 text-grey-600">Second year student at the University of Kent</p>
                 <p className="fade  mt-3 text-2xl">Proficient in Java, Springboot, React, SQL</p>
 
                 <div className="fade mt-8">
