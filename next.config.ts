c70813c454b22da3315becfb6d72b0736c1a27dd
NathanT <nathanthompson256@outlook.com>
2025-10-31 23:11:30 +0000

    added basic education card improvements, just need to generalise it

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/ui/EducationCard.tsx
+++ src/components/ui/EducationCard.tsx
@@ -1,7 +1,8 @@
 'use client';
 import { useState } from 'react';
 import {motion, AnimatePresence} from "framer-motion";
-import {ChevronDown, ChevronUp} from "lucide-react";
+import { ChevronDown, ChevronUp, ExternalLink } from "lucide-react";
+import Avatar from "@/components/ui/Avatar";
 
 export interface EducationCardProps {
     school: string;
@@ -20,9 +21,21 @@ export default function EducationCard({ school, degree, period, description }: E
         >
             <div className="flex justify-between items-center p-6">
                 <div>
-                    <h3 className="text-2xl font-semibold">{school}</h3>
+                    <a
+                        href={"https://www.kent.ac.uk/"}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="group inline-flex items-center text-2xl font-semibold text-white transition-all  hover:underline"
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        {school}
+                        <ExternalLink
+                            className="ml-2 h-4 w-4 opacity-0 translate-x-1 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300"
+                        />
+                    </a>
                     <p className="text-sm text-grey-400 italic">{period}</p>
                 </div>
+
                 {isOpen ? (
                     <ChevronUp className="text-grey-300" />
                 ) : (
@@ -39,11 +52,18 @@ export default function EducationCard({ school, degree, period, description }: E
                         exit={{ height: 0, opacity: 0 }}
                         transition={{ duration: 0.3, ease: "easeInOut" }}
                     >
-                        <div className="px-10 pb-6">
-                            <p className="text-grey-300 text-lg">{degree}</p>
-                            {description && (
-                                <p className="text-grey-400 mt-2">{description}</p>
-                            )}
+                        <div className="flex justify-between items-center px-10 pb-6">
+                            <div>
+                                <p className="text-grey-300 text-lg">{degree}</p>
+                                {description && (
+                                    <p className="text-grey-400 mt-2">{description}</p>
+                                )}
+                            </div>
+                            <Avatar
+                                src="https://media.licdn.com/dms/image/v2/C4E0BAQFm0m0tHm7bsQ/company-logo_200_200/company-logo_200_200/0/1663777988087/university_of_kent_logo?e=1763596800&v=beta&t=80T-trzQdhUNbIoKNB3O6MnKL5bHWgyET-vzpAH-48w"
+                                size={60}
+                                border={true}
+                            />
                         </div>
                     </motion.div>
                 )}
