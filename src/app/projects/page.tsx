4e29787147312dd5317530bc605d95154cdf1ca5
NathanT <nathanthompson256@outlook.com>
2025-10-31 00:11:27 +0000

    Add react-confetti-boom package and integrate into ProjectsPage

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,11 +5,12 @@ import Socials from "@/components/ui/Socials";
 import Education from "@/components/ui/Education";
 import ButtonLink from "@/components/ui/ButtonLink";
 import SectionWrapper from "@/components/layout/SectionWrapper";
+import Confetti from 'react-confetti-boom';
 
 export default function ProjectsPage() {
     return (
-        <div className="mx-auto flex min-h-screen w-full max-w-7xl flex-col gap-10 px-6 py-12 md:flex-row md:items-start md:gap-16 lg:px-16">
-            <main className="flex-1">
+        <div className="mx-auto flex min-h-screen w-full max-w-7xl flex-col gap-10 px-6 py-12 md:flex-row md:items-start md:gap-16 lg:px-16 ">
+            <main className="flex-1 pt-30">
                 <PageWrapper>
                     <SectionWrapper>
                         <h1 className="text-4xl font-bold mb-6">Projects</h1>
